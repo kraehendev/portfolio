45dbeecddd335d49ad85f82e4c6cd25626641b35
kraehendev <email@florian-kuehne.de>
2026-05-17 05:21:52 +0200

    Day one patch, fixing same hydration and iphone ui errors

--- src/components/sections/heroSection.tsx
+++ src/components/sections/heroSection.tsx
@@ -16,16 +16,18 @@ export default async function HeroSection({
 
   return (
     <div
-      className={`relative box-border flex w-full min-h-[calc(80dvh-60px)] items-center justify-center overflow-hidden lg:min-h-[80vh] ${className}`}
+      className={`relative isolate box-border flex w-full min-h-[calc(80svh-60px)] items-center justify-center overflow-x-clip overflow-y-hidden lg:min-h-[80vh] ${className}`}
     >
       <div className="relative z-[2] mx-auto w-full max-w-[60rem] px-4 text-center sm:px-6">
-        <Heading as="h1">
-          {hero('name')} <br />
-          {hero('role')}
-        </Heading>
-        <Heading as="h2" className="mt-4">
-          {common('welcome')}
-        </Heading>
+        <div className="mx-auto w-fit max-w-full rounded-lg bg-black/80 px-5 py-4 md:bg-transparent lg:p-0 lg:rounded-none">
+          <Heading as="h1" className="!mb-4 lg:!mb-8">
+            {hero('name')} <br />
+            {hero('role')}
+          </Heading>
+          <Heading as="h2" className="!mt-0 !mb-0 lg:!mt-4 lg:!mb-8">
+            {common('welcome')}
+          </Heading>
+        </div>
       </div>
       <HeroDecor />
     </div>
